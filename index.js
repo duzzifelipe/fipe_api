8e9fc721cd46f5fbf4b7264f664c4986efa15ee4
Felipe Duzzi <duzzifelipe@gmail.com>
2019-02-09 14:50:41 -0200

    remove proxy

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const async = require('async');
 const { parseMakes, parseDescription } = require('./app/Parsers');
-const ProxyUrl = require('./app/ProxyUrl');
 
 console.log('Starting...');
 
 // create a async handler for getting the makes and
 // after retrieving each car information
 async.waterfall([
-  cb => {
-    console.log('... Getting proxy links ...')
-    // get a proxy link
-    const proxy = new ProxyUrl();
-    proxy.initialize(instance => {
-      global.proxyInstance = instance;
-      console.log(`-> Done with ${instance._urls.length} links`);
-      cb(null);
-    });
-  },
   (cb) => {
     console.log('... Getting makes ...');
     parseMakes(cb);
