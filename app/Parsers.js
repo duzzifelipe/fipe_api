66b90e5ecaec77e34dc904454e40dbad99caa7b5
Felipe Duzzi <duzzifelipe@gmail.com>
2019-02-16 13:02:36 -0200

    created a multiple item foreach

--- app/Parsers.js
+++ app/Parsers.js
@@ -1,5 +1,5 @@
 const { getMakes, getModels, getYears, getDescription } = require('./FipeRequester');
-const { asyncForEach } = require('./Helpers');
+const { asyncForEach, asyncForEachMulti } = require('./Helpers');
 
 // get makes, append to a file
 // and return it to the success callback
